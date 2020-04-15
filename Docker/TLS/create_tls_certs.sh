ce7f63d2c0c36939509de27b95e610b3da736af9
fengchao <chao.feng9@pactera.com>
2020-04-15 09:50:27 +0800

    feat: Docker TLS

---
+++ Docker/TLS/create_tls_certs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#相关配置信息
+SERVER="49.232.60.163"
+HOST="ipomelo.online"
+PASSWORD=""
+COUNTRY="CN"
+STATE="JS"
+CITY="WX"
+ORGANIZATION="PACT"
+ORGANIZATIONAL_UNIT="DEV"
+EMAIL="fengchao.z@outlook.com"
+
+###开始生成文件###
+echo "start..."
+
+#切换到生产密钥的目录
+cd /etc/docker
+#生成ca私钥(使用aes256加密)
+openssl genrsa -aes256 -passout pass:$PASSWORD  -out ca-key.pem 2048
+#生成ca证书，填写配置信息
+openssl req -new -x509 -passin "pass:$PASSWORD" -days 3650 -key ca-key.pem -sha256 -out ca.pem -subj "/C=$COUNTRY/ST=$STATE/L=$CITY/O=$ORGANIZATION/OU=$ORGANIZATIONAL_UNIT/CN=$SERVER/emailAddress=$EMAIL"
+
+#生成server证书私钥文件
+openssl genrsa -out server-key.pem 2048
+#生成server证书签名文件
+openssl req -subj "/CN=$SERVER" -sha256 -new -key server-key.pem -out server.csr
+#使用CA证书及CA密钥以及上面的server证书请求文件进行签发，生成server自签证书
+echo subjectAltName = DNS:$HOST,IP:$SERVER,IP:127.0.0.1 >> extfile.cnf
+echo extendedKeyUsage = serverAuth >> extfile.cnf
+openssl x509 -req -days 3650 -sha256 -in server.csr -CA ca.pem -CAkey ca-key.pem -passin "pass:$PASSWORD" -CAcreateserial -out server-cert.pem -extfile extfile.cnf
+
+#生成client证书RSA私钥文件
+openssl genrsa -out key.pem 2048
+#生成client证书请求文件
+openssl req -subj '/CN=client' -new -key key.pem -out client.csr
+
+echo extendedKeyUsage = clientAuth > extfile-client.cnf
+#生成client自签证书（根据上面的client私钥文件、client证书请求文件生成）
+openssl x509 -req -days 3650 -sha256 -in client.csr -CA ca.pem -CAkey ca-key.pem -passin "pass:$PASSWORD" -CAcreateserial -out cert.pem -extfile extfile-client.cnf
+
+#更改密钥权限
+chmod 0400 ca-key.pem key.pem server-key.pem
+#更改密钥权限
+chmod 0444 ca.pem server-cert.pem cert.pem
+#删除无用文件
+rm client.csr server.csr
+
+#重启Docker
+systemctl daemon-reload
+systemctl restart docker
+
+echo "end"
+###生成结束###
